4ea9a56d70cbb896811b37b4ade55f78285cd5d8
Jorge Vazquez <jorgeomar.vazquez@gmail.com>
2017-12-08 21:42:56 -0300

    Binary search tree

---
+++ binary_search_tree/node.py
@@ -0,0 +1,8 @@
+class Node:
+    def __init__(self, data):
+        """ Representation of a binary tree """
+
+        self.data = data
+        self.left = None
+        self.right = None
+
---
+++ binary_search_tree/search.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Author: Jorge Omar Vazquez <jorgeomar.vazquez@gmail.com>
+"""
+def check_BST(root):
+    pass
+
