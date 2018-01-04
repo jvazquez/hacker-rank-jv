98f1465d9c28b4f874b0af17d843d64adcf7310d
Jorge Omar Vazquez <jorgeomar.vazquez@gmail.com>
2018-01-04 14:20:44 -0300

    Anagram spec

---
+++ tries_contacts/app.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+:mod:`app` -- Description here
+===================================
+
+.. module:: module-name-here
+   :platform: Unix, Windows
+   :synopsis: complete.
+.. moduleauthor:: Jorge Omar Vazquez <jorgeomar.vazquez@gmail.com>
+..:date: Jan 4, 2018
+"""
+
+case = [
+    {"op": "add", "contact": "hack"},
+    {"op": "add", "contact": "hackerrank"},
+    {"op": "find", "contact": "hac"},
+    {"op": "find", "contact": "hak"},
+]
+
+for a0 in range(n):
+    op, contact = input().strip().split(' ')
