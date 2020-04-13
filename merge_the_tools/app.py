4a3da156c0ad0be29bd67022f91d16e24210320d
Jorge Omar Vázquez <jorgeomar.vazquez@gmail.com>
2020-04-13 09:28:14 -0300

    Wip
    Signed-off-by: Jorge Omar Vázquez <jorgeomar.vazquez@gmail.com>

--- merge_the_tools/app.py
+++ merge_the_tools/app.py
@@ -1,6 +1,21 @@
+from itertools import zip_longest
+
+
 def is_factor(number, factor) -> bool:
     return number % factor == 0
 
 
-def splitter(target_string: str, subsegments: int):
-    pass
+def remove_duplicates(sub_segment: str) -> str:
+    buffer = set()
+    output = []
+    for char in sub_segment:
+        if char not in buffer:
+            output.append(char)
+            buffer.add(char)
+    return "".join(output)
+
+
+def groupper(target_string: str, sub_segments: int):
+    args = [iter(target_string)] * sub_segments
+    for sub_segment in zip_longest(*args):
+        yield remove_duplicates(sub_segment)
--- test/merge_the_tools/test_merge_the_tools.py
+++ test/merge_the_tools/test_merge_the_tools.py
@@ -1,14 +1,19 @@
 import unittest
 
-from merge_the_tools.app import is_factor
+from merge_the_tools.app import groupper, is_factor, remove_duplicates
 
 
 class TestMergeTheTools(unittest.TestCase):
     def test_is_factor(self):
         self.assertEqual(True, is_factor(9, 3))
 
-    def test_obtain_number(self):
-        target = "AABCAAADA"
+    def test_remove_duplicates(self):
+        target = "AAB"
+        expected = "AB"
+        output = remove_duplicates(target)
+        self.assertEqual(output, expected, f"Got {output}")
+        # change this to something different, with generators perhaps
+
 
 if __name__ == '__main__':
     unittest.main()
