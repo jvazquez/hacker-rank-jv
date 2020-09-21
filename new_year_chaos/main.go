24e81f9aabff12a62e0724a16f93fdcfaae257cf
Jorge Omar Vázquez <jorgeomar.vazquez@gmail.com>
2020-09-20 21:47:28 -0300

    Wip working but the huge test cases that timeout are consuming too much time even with my local machine

--- new_year_chaos/main.go
+++ new_year_chaos/main.go
@@ -1,9 +1,39 @@
 package main
 
-import "fmt"
+const ErrorMessage = "Too chaotic"
+const MaxShifts = 2
 
 func main() {
-	queue := [5]int32{2, 1, 5, 3, 4}
-	fmt.Printf("All elements: %d\n", queue[0:])
-	fmt.Printf("Cap for an array is %d\n", cap(queue))
+}
+
+func MinimumBribes(consumerLine []int32) (int32, string) {
+	var positionShifts int32
+	var invalidShift bool
+	// fmt.Printf("FS: %v\n", consumerLine)
+
+	for position, person := range consumerLine {
+
+		casesAhead := len(consumerLine[0:position])
+		position++
+		if person-int32(position) > MaxShifts {
+			invalidShift = true
+		}
+
+		if casesAhead != 0 {
+			// fmt.Printf("%d, Lookahead %v\n", person, consumerLine[0:position])
+		}
+
+		for i := 0; i < casesAhead; i++ {
+			if consumerLine[i] > person {
+				positionShifts += 1
+				// fmt.Printf("%d > %d\n", consumerLine[i], person)
+			}
+		}
+	}
+
+	if invalidShift {
+		return 0, ErrorMessage
+	} else {
+		return positionShifts, ""
+	}
 }
